6752e69b17c39be93f3bd1bb872efca07cc950e2
Oleksandra Baga <oleksandra.baga@gmail.com>
2020-06-10 12:51:45 +0200

    added inverse function for DTC-II

--- discrete_cosine_transformation_type_2.py
+++ discrete_cosine_transformation_type_2.py
@@ -31,3 +31,45 @@ def dct2d(img):
     return second
  
 
+def inv_dct1d(line):
+
+    N = len(line)
+    result = np.zeros(N, dtype=float)
+    
+    ns = np.arange(1, N)
+    for k in range(N):
+        result[k] = (0.5 * line[0]) + np.sum(line[ns]*np.cos((np.pi*(k+0.5)*ns)/N))
+        
+    return result 
+    
+    
+def inv_dct2d(x):
+    """
+    Returns the 2d inverse discrete cosine transformation
+    The inverse of DCT-II is DCT-III multiplied by 2/N an
+    """
+    # your code here
+    N1 = x.shape[0]
+    N2 = x.shape[1]
+    first = np.ones([N1, N2], dtype=float)
+    second = np.ones([N1, N2], dtype=float)
+      
+    first = np.apply_along_axis(inv_dct1d, 0, x)
+    second = np.apply_along_axis(inv_dct1d, 1, first)
+    return second
+
+def chess_board(n=8, field_size=32):
+    board = np.zeros((n*field_size, n*field_size))
+    s = field_size
+    for i in range(n):
+        for j in range(n):
+            if (i + j) % 2 == 0:
+                board[i*s:(i+1)*s, j*s:(j+1)*s] = 1
+    return board
+
+
+
+for pic in [chess_board(), rgb2gray(chelsea())/255]:
+    #plt.imshow(dct2d(pic), cmap='gray')
+    plt.imshow(inv_dct2d(dct2d(pic)), cmap='gray')
+    plt.show()
